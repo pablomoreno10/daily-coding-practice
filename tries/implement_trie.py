a4fce3a2de379ac5e600c866b8faed30ec0c6355
Pablo Moreno <pablonicolasmc5@gmail.com>
2025-04-18 23:10:47 -0400

    Create implement_trie.py

---
+++ tries/implement_trie.py
@@ -0,0 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {} 
+        self.isEndWord = False
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:  
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isEndWord = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.isEndWord
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
